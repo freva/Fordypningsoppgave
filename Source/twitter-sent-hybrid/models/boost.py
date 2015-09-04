f5e063d54ee054af10da0f0c7a0ffff12e647b08
Limon <valerijf@stud.ntnu.no>
2015-09-04 18:09:01 +0200

    Added initial hybrid version (BrevikSelmer without server)

---
+++ Source/twitter-sent-hybrid/models/boost.py
@@ -0,0 +1,80 @@
+"""
+    Sentiment analysis using basic bigrams.
+"""
+import logging
+
+from base import BaseMethod
+from numpy import mean
+from utils.utils import translate_to_numbers, translate_from_number
+from models import *
+from itertools import product
+from random import choice
+import numpy as np
+
+class Boosting(BaseMethod):
+    
+  def __init__(self, docs_train, y_train, docs_train_subjectivity=None, y_train_subjectivity=None, docs_train_polarity=None, y_train_polarity=None):
+    nb = NB
+    svm = SVM
+    maxent = MaxEnt
+
+    self.baseModels = [nb, svm, maxent]
+
+    self.models = [ 
+      nb(docs_train, y_train),
+      svm(docs_train, y_train),
+      maxent(docs_train, y_train) 
+    ]
+
+    if docs_train_subjectivity != None:
+      self.combination_permutation(docs_train_subjectivity, y_train_subjectivity, docs_train_polarity, y_train_polarity)
+
+    self.set_best_score()
+
+
+  def combination_permutation(self, docs_train_subjectivity, y_train_subjectivity, docs_train_polarity, y_train_polarity):
+    perms = product(self.baseModels, repeat=2)
+    for cmb in perms:
+      c1 = cmb[0](docs_train_subjectivity, y_train_subjectivity)
+      c2 = cmb[1](docs_train_polarity, y_train_polarity)
+      self.models.append(Combined(c1, c2))
+
+
+  def set_best_score(self):
+    self.best_score = np.mean(map(lambda x: x.best_score, self.models))
+
+
+  def train(self):
+    pass
+
+  def pick_majoraty(self, y_pred):
+    clfs = {}
+    for i, m in zip(y_pred, self.models):
+      # mavg = m.best_score / len(self.models)
+      mavg = m.best_score
+      clfs[i] = clfs[i] + mavg  if i in clfs else mavg
+
+    highest_val = max(clfs.values())
+    highest = filter(lambda t: t[1] == highest_val, clfs.items())
+    return choice(highest)[0]
+
+  def predict_list(self, li):
+    y_predictions = []
+    for i in li:
+      prediction = self.predict(i)
+      y_predictions.append(prediction)
+
+    return y_predictions
+
+  def predict(self, arg_input):
+    
+    if not isinstance(arg_input, basestring):
+      return self.predict_list(arg_input)
+
+    y_pred = []
+    for m in self.models:
+      y_pred.append(m.predict(arg_input))
+
+    pred = self.pick_majoraty(y_pred)
+    return pred
+
---
+++ Source/twitter-sent-hybrid/models/combination.py
@@ -0,0 +1,37 @@
+"""
+    Sentiment analysis using basic bigrams.
+"""
+import logging
+
+from base import BaseMethod
+
+class Combined(BaseMethod):
+    
+  def __init__(self, m1, m2):
+    self.subjectivity_clf = m1
+    self.polarity_clf = m2
+
+    self.best_score = (self.subjectivity_clf.best_score + self.polarity_clf.best_score) / 2
+
+  def train(self, docs_train_subjectivity, y_train_subjectivity, docs_train_polarity, y_train_polarity):
+    pass
+
+  def predict_list(self, li):
+    y_predicted = []
+    for i in li:
+      y_predicted.append(self.predict(i))
+    return y_predicted
+
+  def predict(self, arg_input):
+    if not isinstance(arg_input, basestring):
+      return self.predict_list(arg_input)
+
+    p1 = self.subjectivity_clf.predict(arg_input)
+    if p1 == 'neutral':
+      return 'neutral'
+
+    return self.polarity_clf.predict(arg_input)
+
+
+  def __str__(self):
+    return "Comb. (%s->%s)" % (str(self.subjectivity_clf), str(self.polarity_clf))
