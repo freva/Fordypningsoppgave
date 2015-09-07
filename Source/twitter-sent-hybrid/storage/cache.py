e55c0137883719a4fdb6728fd45ad3a6940cfbde
freva <valerijf@stud.ntnu.no>
2015-09-07 13:40:09 +0200

    Cleaned up storage

--- Source/twitter-sent-hybrid/storage/cache.py
+++ Source/twitter-sent-hybrid/storage/cache.py
@@ -6,30 +6,33 @@ import pickle
 from os import path
 
 dir_path = "pickles/"
-
 training_filename = ""
 
-def set_training_file(filename):
-  global training_filename
-  training_filename = filename
 
-def generate_filename (str_id):
-  filename = md5(str(str_id) + training_filename).hexdigest()
-  return str(dir_path + filename + ".pkl")
+def set_training_file(filename):
+    global training_filename
+    training_filename = filename
+
+
+def generate_filename(str_id):
+    filename = md5(str(str_id) + training_filename).hexdigest()
+    return str(dir_path + filename + ".pkl")
+
 
 def save(str_id, obj):
-  full_path = generate_filename(str_id)
-  output = open(full_path, 'wb')
-  pickle.dump(obj, output)
-  output.close()
+    full_path = generate_filename(str_id)
+    output = open(full_path, 'wb')
+    pickle.dump(obj, output)
+    output.close()
 
-  return obj
+    return obj
+
 
 def get(str_id):
-  full_path = generate_filename(str_id)
-  if not path.exists(full_path):
-    return False
+    full_path = generate_filename(str_id)
+    if not path.exists(full_path):
+        return False
 
-  # check if classifier is saved at ~/.sentimetn/classifier.pickle of some sort
-  pkl_file = open(full_path, 'rb')
-  return pickle.load(pkl_file)
+    # check if classifier is saved at ~/.sentimetn/classifier.pickle of some sort
+    pkl_file = open(full_path, 'rb')
+    return pickle.load(pkl_file)
--- Source/twitter-sent-hybrid/storage/data.py
+++ Source/twitter-sent-hybrid/storage/data.py
@@ -1,4 +1,3 @@
-import sys
 import numpy as np
 
 from os import path
@@ -17,39 +16,38 @@ train = None
 test = None
 
 def set_file_names(train_set = None, test_set = None):
-  global train, test
-  train_set_filename = (train_set if train_set != None else False) or '../Testing/data/tweeti-b.dist.output.tsv'
-  test_set_filename = (test_set if test_set != None else False) or '../Testing/data/test/twitter-dev-gold-B.tsv'
-  # test_set_filename = (test_set if test_set != None else False) or 'data/test/test_output_tweets.tsv'
-  cache.set_training_file(train_set_filename)
+    global train, test
+    train_set_filename = (train_set if train_set != None else False) or '../Testing/data/output2.tsv'
+    test_set_filename = (test_set if test_set != None else False) or '../Testing/data/test/twitter-dev-gold-B.tsv'
+    # test_set_filename = (test_set if test_set != None else False) or 'data/test/test_output_tweets.tsv'
+    cache.set_training_file(train_set_filename)
 
-  if not path.exists(train_set_filename) or not path.exists(test_set_filename): # or not path.exists(test_set_filename):
-    raise Exception("File not found")
+    if not path.exists(train_set_filename) or not path.exists(test_set_filename): # or not path.exists(test_set_filename):
+        raise Exception("File not found")
 
-  train = np.loadtxt(train_set_filename, delimiter='\t', dtype='S', comments=None)
-  test = np.loadtxt(test_set_filename, delimiter='\t', dtype='S', comments=None)
+    train = np.loadtxt(train_set_filename, delimiter='\t', dtype='S', comments=None)
+    test = np.loadtxt(test_set_filename, delimiter='\t', dtype='S', comments=None)
 
 
 def get_full_test_set():
-  global test
-  return test
+    global test
+    return test
 
 def get_data():
-  global train, test
+    global train, test
 
-  test = u.normalize_test_set_classification_scheme(test)
-  train = u.normalize_test_set_classification_scheme(train)
+    test = u.normalize_test_set_classification_scheme(test)
+    train = u.normalize_test_set_classification_scheme(train)
 
-  # Normalize data?
-  train = u.reduce_dataset(train, 3000)
+    # Normalize data?
+    train = u.reduce_dataset(train, 3000)
 
-  # To compansate for poor TSV data structure
-  i_d = 4 if len(test[0]) > 4 else 3
-  t_d = 4 if len(train[0]) > 4 else 3
+    # To compansate for poor TSV data structure
+    i_d = 4 if len(test[0]) > 4 else 3
+    t_d = 4 if len(train[0]) > 4 else 3
 
-  docs_test, y_test = test[:,i_d], test[:,i_d-1]
-  docs_train, y_train = train[:,t_d], train[:,t_d-1]
+    docs_test, y_test = test[:,i_d], test[:,i_d-1]
+    docs_train, y_train = train[:,t_d], train[:,t_d-1]
 
-
-  docs_train_subjectivity, y_train_subjectivity, docs_train_polarity, y_train_polarity = u.generate_two_part_dataset(train)
-  return docs_test, y_test, docs_train, y_train, docs_train_subjectivity, y_train_subjectivity, docs_train_polarity, y_train_polarity
+    docs_train_subjectivity, y_train_subjectivity, docs_train_polarity, y_train_polarity = u.generate_two_part_dataset(train)
+    return docs_test, y_test, docs_train, y_train, docs_train_subjectivity, y_train_subjectivity, docs_train_polarity, y_train_polarity
