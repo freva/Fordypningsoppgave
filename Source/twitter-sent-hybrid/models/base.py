2892cb64a5c8ecaa2795a0635583d2e0f45792a0
freva <valerijf@stud.ntnu.no>
2015-09-07 14:28:32 +0200

    Cleaned up main

--- Source/twitter-sent-hybrid/models/base.py
+++ Source/twitter-sent-hybrid/models/base.py
@@ -1,12 +1,11 @@
 """
     Base class for different methods of using sentiment analysis.
 """
-import logging
 import sys
 from sklearn.pipeline import Pipeline
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cross_validation import train_test_split, StratifiedKFold
+from sklearn.cross_validation import StratifiedKFold
 from sklearn.grid_search import GridSearchCV
 
 import utils.preprocessor_methods as pr
@@ -15,24 +14,26 @@ from storage import cache
 
 
 class BaseMethod(object):
-    def __init__(self, docs_train, y_train, extra = {}, useCrossValidation = False, vect_options = {}):
+    def __init__(self, docs_train, y_train, extra={}, useCrossValidation=False, vect_options={}):
         if sys.flags.debug:
             self.options = {}
         else:
             self.options = {
-                'vect__ngram_range': [(1, 1)], # (2, 2), (3,3)],
-                # 'vect__stop_words': ('english', None),
-                'vect__preprocessor': (None, pr.no_prep, pr.no_usernames, pr.remove_noise, pr.placeholders, pr.all, pr.remove_all, pr.reduced_attached, pr.no_url_usernames_reduced_attached),
+                'vect__ngram_range': [(1, 1)],  # (2, 2), (3,3)],
+                #'vect__stop_words': ('english', None),
+                'vect__preprocessor': (
+                None, pr.no_prep, pr.no_usernames, pr.remove_noise, pr.placeholders, pr.all, pr.remove_all,
+                pr.reduced_attached, pr.no_url_usernames_reduced_attached),
                 'vect__use_idf': (True, False),
                 'vect__max_df': (0.5,),
                 'vect__smooth_idf': (True, False),
                 'vect__sublinear_tf': (True, False)
-              }
+            }
 
         self.train(docs_train, y_train, extra, useCrossValidation, vect_options)
 
 
-    def train(self, docs_train, y_train, extra = {}, useCrossValidation = False, vect_options={}):
+    def train(self, docs_train, y_train, extra={}, useCrossValidation=False, vect_options={}):
         options = dict(self.options.items() + extra.items())
         cv = StratifiedKFold(y_train, n_folds=10) if useCrossValidation else None
 
@@ -45,27 +46,23 @@ class BaseMethod(object):
 
         if useGrid:
             self.grid = GridSearchCV(
-                pipeline, 
+                pipeline,
                 options,
                 cv=cv,
                 refit=True,
                 n_jobs=-1,
                 verbose=1
-              )
+            )
         else:
             self.grid = pipeline
 
         cache_key = str(self.grid) + str(docs_train)
         cached = cache.get(cache_key)
-    
+
         if cached and sys.flags.debug == 0:
-            logging.debug("# Fetched cached version of %s " % self.clf.__class__.__name__)
             self.best_estimator = cached['est']
             self.best_score = cached['scr']
             self.best_params = cached['parm']
-
-        else:
-            logging.debug("# Training new instance of %s " % self.clf.__class__.__name__)
 
         self.grid.fit(docs_train, y_train)
 
@@ -78,7 +75,6 @@ class BaseMethod(object):
             self.best_params = self.grid.get_params(False)
             self.best_score = 1
 
-            logging.debug("Saving to cache for %s " % self.clf.__class__.__name__)
             cache.save(cache_key, {
                 "est": self.best_estimator,
                 "scr": self.best_score,
@@ -86,14 +82,8 @@ class BaseMethod(object):
             })
 
         self.steps = self.best_estimator.named_steps
-
-        logging.debug("# Best params for  %s :" % self.clf.__class__.__name__)
-        logging.debug(self.best_params)
-
-        logging.debug("# Best score for  %s :" % self.clf.__class__.__name__)
-        logging.debug(self.best_score)
-
         return self.grid
+
 
     def predict(self, arg_input):
         orig = arg_input
@@ -105,6 +95,7 @@ class BaseMethod(object):
             return predictions[0]
 
         return predictions
+
 
     def __str__(self):
         return "%s" % self.__class__.__name__
