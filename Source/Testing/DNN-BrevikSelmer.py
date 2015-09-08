785cae4866e1da5f2ca0d896c147995d4b7e86dc
freva <valerijf@stud.ntnu.no>
2015-09-08 12:35:34 +0200

    Fixed tests

--- Source/Testing/DNN-BrevikSelmer.py
+++ Source/Testing/DNN-BrevikSelmer.py
@@ -1,9 +1,10 @@
 from json import dumps
 import requests, sys
 
+
 def sendPOSTRequest(text):
     urlLocal = 'http://localhost:7000/'
-    urlRemote = 'http://ntnutwitsent.herokuapp.com:7000/'    
+    urlRemote = 'http://ntnutwitsent.herokuapp.com:7000/'
     return requests.post(urlLocal, data=text)
 
 
@@ -11,20 +12,20 @@ def sentimentFromCommandLine():
     data = ' '.join(sys.argv[1:])
     request = []
     for line in data.split(","):
-        request.append(dumps({"text":  line}))
+        request.append(dumps({"text": line}))
 
     response = sendPOSTRequest('\n'.join(request))
     print response.text
-    
+
 
 def testFile(filename):
     data = [i.split("\t") for i in open(filename, "r").read().split("\n")]
 
     request, classifications = [], []
     for tweetID, unknown, classification, text in data:
-        request.append(dumps({"text":  text}))
-        c = classification[1:-1]
-        
+        request.append(dumps({"text": text}))
+        c = classification
+
         if "objective" in c:
             c = "neutral"
         classifications.append(c)
@@ -32,11 +33,13 @@ def testFile(filename):
     response = sendPOSTRequest('\n'.join(request))
     response = [i for i in response.text.split("\n") if len(i) > 0]
 
-    print sum([1 if a==b else 0 for a, b in zip(classifications, response)])
+    cor, tot = sum([a == b for a, b in zip(classifications, response)]), len(classifications)
+    print cor, tot, ("%.2f" % (100.0 * cor / tot))
 
 
 def dnnTestFile(filename):
     from sentiment import sentiment_score
+
     data, numCorrect = [i.split("\t") for i in open(filename, "r").read().split("\n")], 0
     i = 0
     positiveTarget, negativeTarget = 0.7, 0.3
@@ -44,19 +47,18 @@ def dnnTestFile(filename):
     response, classifications = [], []
     for tweetID, unknown, classification, text in data:
         print i
-        i+=1
-        
+        i += 1
+
         sentval = sentiment_score(text.decode("utf-8"))
         if classification == "positive":
-            if sentval >= postiveTarget: numCorrect += 1
+            if sentval >= positiveTarget: numCorrect += 1
         elif classification == "negative":
-            if sentval <= nevativeTarget: numCorrect += 1
+            if sentval <= negativeTarget: numCorrect += 1
         else:
             if negativeTarget < sentval < positiveTarget: numCorrect += 1
     print numCorrect
-        
 
-#testFile("test.tsv")
-#dnnTestFile("test.tsv")
-sentimentFromCommandLine()
 
+testFile("data/test/twitter-dev-gold-B.tsv")
+# dnnTestFile("test.tsv")
+# sentimentFromCommandLine()
