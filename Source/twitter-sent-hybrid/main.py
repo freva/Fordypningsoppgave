392bcbfc9a8331d61ed4f9e72870db54db86c19e
freva <valerijf@stud.ntnu.no>
2015-09-24 13:02:49 +0200

    Something

--- Source/twitter-sent-hybrid/main.py
+++ Source/twitter-sent-hybrid/main.py
@@ -4,6 +4,7 @@
     Takes POST requests and returns a string with the classification.
     Classification scheme: <neutral, positive, negative>
 """
+import argparse
 import importlib
 import time
 start_time = time.clock()
@@ -38,7 +39,13 @@ clf = SVM(docs_train, y_train, default_options=c1_default_options, vect_options=
 #clf = Boosting(docs_train, y_test)
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
+    if len(sys.argv) == 3 and sys.argv[1] == "test":
+        importlib.import_module("test." + sys.argv[2])
+
+    else:
+        sys.argv = ["-O"]
+        print sys.flags
+
         results, out = defaultdict(lambda: []), []
 
         for tweet, classification in zip(docs_test, y_test):
@@ -56,12 +63,5 @@ if __name__ == "__main__":
         out.extend(["%.2f" % (100.0 * totCor / totTot), "%.2f" % (time.clock()-start_time)])
         print "total", str(totCor).rjust(5), str(totTot).rjust(5), out[-1].rjust(5)
         print '\t'.join(out)
-
-    elif len(sys.argv) == 3 and sys.argv[1] == "test":
-        importlib.import_module("test." + sys.argv[2])
-
-    else:
-        text = ' '.join(sys.argv[1:])
-        print(clf.predict(text))
 
 print "In", "%.2f" % (time.clock()-start_time), "sec"
