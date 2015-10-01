ebe4f8f4573041ab748628e02b132daeec3853e3
freva <valerijf@stud.ntnu.no>
2015-10-01 11:14:57 +0200

    Renamed feature to transformer

--- Source/twitter-sent-hybrid/transformer/__init__.py
+++ Source/twitter-sent-hybrid/transformer/__init__.py
@@ -1,3 +1,3 @@
 # Here we need to list all the different features extractors we have.
-from .clusters import ClusterTransformer
+from .clusters_transformer import ClusterTransformer
 from .word_counter import WordCounter
