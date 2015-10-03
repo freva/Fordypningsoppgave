d5b1530e69e7dd158cecfa712f4bd9acea68425a
Limon <valerijf@stud.ntnu.no>
2015-10-03 21:21:07 +0200

    Removed word_counter

--- Source/twitter-sent-hybrid/transformer/__init__.py
+++ Source/twitter-sent-hybrid/transformer/__init__.py
@@ -1,6 +1,5 @@
 # Here we need to list all the different features extractors we have.
 from .clusters_transformer import ClusterTransformer
-from .word_counter import WordCounter
 from .allcaps_transformer import AllcapsTransformer
 from .elongation_transformer import ElongationTransformer
 from .punctuation_transformer import PunctuationTransformer
