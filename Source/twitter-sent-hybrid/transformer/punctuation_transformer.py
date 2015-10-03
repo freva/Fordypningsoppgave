9d43752b4022550328aa7b08cf3afeb598898478
Limon <valerijf@stud.ntnu.no>
2015-10-03 21:21:26 +0200

    Improved punctuation

--- Source/twitter-sent-hybrid/transformer/punctuation_transformer.py
+++ Source/twitter-sent-hybrid/transformer/punctuation_transformer.py
@@ -1,36 +1,22 @@
-import re
-
 from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.preprocessing import normalize
-import numpy as np
-from nltk.tokenize import wordpunct_tokenize
 
 
 class PunctuationTransformer(TransformerMixin, BaseEstimator):
-    def __init__(self, norm=True):
+    def __init__(self, norm=True, preprocessor=None):
         self.normalize = norm
+        self.preprocessor = preprocessor
 
     def fit(self, raw_tweets, y=None):
         return self
 
     def transform(self, raw_tweets):
-        vectorized = np.zeros((len(raw_tweets), 4))
+        vectorized, search = [], ["!", "?", "\""]
         for i, tweet in enumerate(raw_tweets):
-            exclamations = 0
-            questions = 0
-            both = 0
-            wordArray = wordpunct_tokenize(tweet)
-            for word in wordArray:
-                word = re.sub(r"[,.]", "", word)
-                if re.match(r"\!{2,}$", word):
-                    exclamations += 1
-                if re.match(r"\?{2,}$", word):
-                    questions += 1
-                if re.match(r"(?:\!+\?+)|(?:\?+\!+)[?!]*", word):
-                    both += 1
-            vectorized[i][0] = exclamations
-            vectorized[i][1] = questions
-            vectorized[i][2] = both
-            vectorized[i][3] = 1 if re.match(r"[!?]{2,}", wordArray[-1]) else 0
+            if self.preprocessor:
+                tweet = self.preprocessor(tweet)
+
+            vectorized.append([float(tweet.count(i)) for i in search])
+            #if sum(vectorized[-1]): print vectorized[-1], tweet
         return normalize(vectorized) if self.normalize else vectorized
 
