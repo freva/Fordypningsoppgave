9cde188d876260661baed91e8aa447370acf265e
Limon <valerijf@stud.ntnu.no>
2015-10-10 23:29:37 +0200

    Added more grid_search parameters

--- Source/twitter-sent-hybrid/grid_search.py
+++ Source/twitter-sent-hybrid/grid_search.py
@@ -18,22 +18,38 @@ def grid_search(clf, feature_pipeline, docs_train, y_train):
     ])
 
     parameters = {
-        'clf__kernel': ('linear', 'poly', 'rbf', 'sigmoid'),
-        'clf__C': (0.5, 1.0),
-        'clf__gamma': (0.001, 0.1, 0.02),
-        'features__punctuation__preprocessor': (pr.html_decode, pr.no_url_username),
-        'features__punctuation__norm': (True, False),
-        'features__emoticons__preprocessor': [pr.html_decode],
-        'features__emoticons__norm': (True, False),
-        'features__tags__preprocessor': [pr.html_decode],
-        'features__tags__norm': (True, False)
+        'clf__kernel': ('linear', 'rbf', 'sigmoid'),
+        'clf__C': (0.5, 0.75, 1.0, 1.25),
+        'clf__gamma': (0.001, 0.1, 0.3, 0.5),
+        'features__word_vectorizer__ngram_range': [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5)],
+        'features__word_vectorizer__sublinear_tf': (True, False),
+        'features__word_vectorizer__use_idf': (True, False),
+        'features__word_vectorizer__smooth_idf': (True, False),
+        'features__word_vectorizer__max_df': (0.25, 0.5, 0.75, 1),
+        'features__word_vectorizer__max_features': (None, 100000, 200000, 300000),
+        'features__word_vectorizer__preprocessor': (pr.remove_all, pr.remove_noise),
+        'features__char_ngrams__ngram_range': [(2, 5), (3, 5), (3, 6), (3, 7)],
+        'features__char_ngrams__sublinear_tf': (True, False),
+        'features__char_ngrams__use_idf': (True, False),
+        'features__char_ngrams__smooth_idf': (True, False),
+        'features__char_ngrams__max_df': (0.25, 0.5, 0.75, 1),
+        'features__char_ngrams__max_features': (None, 100000, 200000, 300000),
+        'features__char_ngrams__preprocessor': (pr.remove_all, pr.remove_noise),
+        'features__word_clusters__preprocessor': [pr.html_decode],      #Dictionary contains hashtags, emotes and even usernames. Dont think any further filtering is possible
+        'features__word_clusters__norm': (True, False),
+        'features__punctuation__preprocessor': (pr.html_decode, pr.no_url_username),    #On average better result with html_decode, but logically doesnt make sense
+        'features__punctuation__norm': [True],              #No preference
+        'features__emoticons__preprocessor': [pr.html_decode],  #Technically could run with no_url_username //overlap with tags
+        'features__emoticons__norm': (True, False),         #0.01% difference in favor of True
+        'features__tags__preprocessor': [pr.html_decode],   #Counts links, no real pre-processing needed
+        'features__tags__norm': [True]                      #No preference
     }
 
     grid = GridSearchCV(
         pipeline,
         param_grid=parameters,
-        scoring=make_scorer(f1_score, pos_label="neutral", average='binary'),
-        cv=StratifiedKFold(y_train, 2),#, shuffle=True), #IDI maskinen sin scikit-learn stotter ikke shuffle (gammel versjon)
+        scoring=make_scorer(precision_score, pos_label="neutral", average='binary'),
+        cv=StratifiedKFold(y_train, 5),#, shuffle=True), #IDI maskinen sin scikit-learn stotter ikke shuffle (gammel versjon)
         n_jobs=-1,
         verbose=10
     )
