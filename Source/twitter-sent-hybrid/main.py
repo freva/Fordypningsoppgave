ade251fd5efe00d95b9c45de64f4ddbbb7b3d53d
Limon <valerijf@stud.ntnu.no>
2015-10-11 19:36:42 +0200

    Removed testing

--- Source/twitter-sent-hybrid/main.py
+++ Source/twitter-sent-hybrid/main.py
@@ -1,8 +1,6 @@
-import importlib
 import time
 
 start_time = time.clock()
-import sys
 import numpy as np
 from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, confusion_matrix
 
@@ -16,32 +14,29 @@ from models.combination import Combined
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3 and sys.argv[1] == "test":
-        importlib.import_module("test." + sys.argv[2])
-    else:
-        train, test = d.get_data(General.TRAIN_SET, General.TEST_SET)
-        docs_test, y_test = test[:,0], test[:,1]
+    train, test = d.get_data(General.TRAIN_SET, General.TEST_SET)
+    docs_test, y_test = test[:,0], test[:,1]
 
-        clf = BaseMethod(train, **SubjectivityFeatures.CLASSIFIER)
-        #clf = Combined(SubjectivityFeatures.CLASSIFIER, PolarityFeatures.CLASSIFIER, train)
-        print "Finished training in", "%.2f" % (time.clock()-start_time), "sec"
-        y_pred = clf.predict(docs_test)
+    clf = BaseMethod(train, **SubjectivityFeatures.CLASSIFIER)
+    #clf = Combined(SubjectivityFeatures.CLASSIFIER, PolarityFeatures.CLASSIFIER, train)
+    print "Finished training in", "%.2f" % (time.clock()-start_time), "sec"
+    y_pred = clf.predict(docs_test)
 
-        """out = ["%.2f" % (100*precision_score(y_test, y_pred, pos_label="neutral", average='binary')),
-            "%.2f" % (100*recall_score(y_test, y_pred, pos_label="neutral", average='binary')),
-            "%.2f" % (100*f1_score(y_test, y_pred, pos_label="neutral", average='binary')),
-            "%.2f" % (100*accuracy_score(y_test, y_pred)),
-            "%.2f" % (time.clock()-start_time)]
-        print '\t'.join(out)"""
+    """out = ["%.2f" % (100*precision_score(y_test, y_pred, pos_label="neutral", average='binary')),
+        "%.2f" % (100*recall_score(y_test, y_pred, pos_label="neutral", average='binary')),
+        "%.2f" % (100*f1_score(y_test, y_pred, pos_label="neutral", average='binary')),
+        "%.2f" % (100*accuracy_score(y_test, y_pred)),
+        "%.2f" % (time.clock()-start_time)]
+    print '\t'.join(out)"""
 
-        out = ["%.2f" % (100*precision_score(y_test, y_pred, pos_label=None, average='macro')),
-            "%.2f" % (100*recall_score(y_test, y_pred, pos_label=None, average='macro')),
-            "%.2f" % (100*f1_score(y_test, y_pred, pos_label=None, average='macro')),
-            "%.2f" % (100*accuracy_score(y_test, y_pred)),
-            "%.2f" % (time.clock()-start_time)]
-        print '\t'.join(out)
+    out = ["%.2f" % (100*precision_score(y_test, y_pred, pos_label=None, average='macro')),
+        "%.2f" % (100*recall_score(y_test, y_pred, pos_label=None, average='macro')),
+        "%.2f" % (100*f1_score(y_test, y_pred, pos_label=None, average='macro')),
+        "%.2f" % (100*accuracy_score(y_test, y_pred)),
+        "%.2f" % (time.clock()-start_time)]
+    print '\t'.join(out)
 
-        C = confusion_matrix(y_test, y_pred)
-        np.set_printoptions(formatter={'float': '{: 0.2f}'.format})
-        print 100.0*C / np.sum(C)
+    C = confusion_matrix(y_test, y_pred)
+    np.set_printoptions(formatter={'float': '{: 0.2f}'.format})
+    print 100.0*C / np.sum(C)
     print "In", "%.2f" % (time.clock()-start_time), "sec"
