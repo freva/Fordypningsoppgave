69f87f868a9b8f5b1f663181b20021668cb4d344
freva <valerijf@stud.ntnu.no>
2015-10-13 15:46:49 +0200

    Removed tag transformer

--- Source/twitter-sent-hybrid/transformer/__init__.py
+++ Source/twitter-sent-hybrid/transformer/__init__.py
@@ -2,6 +2,5 @@
 from .clusters_transformer import ClusterTransformer
 from .punctuation_transformer import PunctuationTransformer
 from .emoticon_transformer import EmoticonTransformer
-from .tag_transformer import TagTransformer
 from .pos_transformer import POSTransformer
 from .lexicon_transformer import LexiconTransformer
