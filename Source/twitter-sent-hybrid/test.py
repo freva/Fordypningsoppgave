f136b2ff66696330fc5c3d010d6475d7458b8721
brageej <brageej@stud.ntnu.no>
2015-10-23 18:13:18 +0200

    Fixed something

--- Source/twitter-sent-hybrid/test.py
+++ Source/twitter-sent-hybrid/test.py
@@ -1,16 +1,21 @@
 from sklearn.metrics import classification_report
 from negation_classifier.negation_scope.twitter_negation_reader import parse_twitter_negation
 from negation_classifier.cue_detector import CueDetector
+from storage.neg_cacher import NegCacher
+from storage import data
+from storage.options import General
+from negation_classifier import crf_estimator
 
 def evaluate():
     # _, brown_dict = load_clusters()
-    detector = CueDetector()
-    tweets = parse_twitter_negation()
-    tokens = [[token for token, _, _, _, _ in tweet]
-              for tweet in tweets]
-    cues = [is_cue for tweet in tweets for _, _, _, is_cue, _ in tweet]
-    pred_cues = [is_cue for tweet in detector.predict(tokens) for is_cue in tweet]
-    print(classification_report(cues, pred_cues, digits=3))
+    train, test = data.get_data(General.TRAIN_SET, General.TEST_SET)
+    test_tweets = [tweet.decode("utf-8") for tweet in test[:,0][:10]]
+    NegCacher.cache(test_tweets)
+    for tweet in test_tweets:
+        print tweet
+        print NegCacher.cached[tweet]
+        print
+
 
 
 if __name__ == "__main__":
