cb560971dd2a7770724db27a7d0fb55a65d2aa40
Limon <valerijf@stud.ntnu.no>
2015-11-04 13:01:40 +0100

    Updated with optimal polarity classifier options

--- Source/twitter-sent-hybrid/storage/resource_reader.py
+++ Source/twitter-sent-hybrid/storage/resource_reader.py
@@ -87,8 +87,8 @@ def get_data(train_set, test_set):
     #test = u.generate_subjective_set(test)
     #train = u.generate_subjective_set(train)
 
-    test = u.generate_polarity_set(test)
-    train = u.generate_polarity_set(train)
+    #test = u.generate_polarity_set(test)
+    #train = u.generate_polarity_set(train)
 
     # Normalize data?
     #train = u.reduce_dataset(train, 3000)
